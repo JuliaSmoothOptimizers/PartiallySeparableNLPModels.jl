430851d60f667809ce304e82c9a7e3984062a19c
paraynaud <paraynaud@users.noreply.github.com>
2022-07-18 15:11:23 +0000

    :robot: Format .jl files

--- test/partitoned_data/PartiallySeparableNLPModel.jl
+++ test/partitoned_data/PartiallySeparableNLPModel.jl
@@ -14,11 +14,10 @@
   @test NLPModels.grad(nlp, x) ≈ NLPModels.grad(pbfgsnlp, x)
   @test NLPModels.grad(nlp, x) ≈ NLPModels.grad(plbfgsnlp, x)
 
-  s = (si -> 0.5*si).(ones(n))
+  s = (si -> 0.5 * si).(ones(n))
   B_pbfgs = update_nlp(pbfgsnlp, x, s)
   B_plbfgs = update_nlp(plbfgsnlp, x, s)
   py = get_py(pbfgsnlp.part_data)
   build_v!(py)
   y = get_v(py)
-
 end
