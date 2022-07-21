cd0ac61310bda7385b8abfb514b770acae07a31e
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-07-21 18:44:21 -0300

    benchmmark -> benchmark

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -2,7 +2,7 @@ using PkgBenchmark
 using SolverBenchmark
 
 commit = benchmarkpkg("PartiallySeparableNLPModels")  #dernier commit sur la branche sur laquelle on se trouve
-master = benchmarkpkg("PartiallySeparableNLPModels", "benchmmark") # branche master
+master = benchmarkpkg("PartiallySeparableNLPModels", "benchmark") # branche master
 judgement = judge(master, commit)
 
 export_markdown("benchmark/judgement.md", judgement)
