e4f8a9df1bbc3682f5df3f0b5975482ae1a55a48
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2023-03-31 12:54:52 -0300

    add tests for PartitionedBackends + hessop

--- src/backends/PartitionedBackends.jl
+++ src/backends/PartitionedBackends.jl
@@ -5,7 +5,7 @@ using NLPModels
 using ExpressionTreeForge, PartitionedVectors, PartitionedStructures
 using ..ModAbstractPSNLPModels
 
-export AbstractObjectiveBackend, AbstractGradientBackend, AbstractHprodBackend
+export PartitionedBackend, AbstractObjectiveBackend, AbstractGradientBackend, AbstractHprodBackend
 export objective, partitioned_gradient!, partitioned_hessian_prod!
 
 abstract type PartitionedBackend{T} end
@@ -19,7 +19,7 @@ abstract type AbstractHprodBackend{T} <: PartitionedBackend{T} end
 
 Compute the objective value from `backend` at the point `x`.
 """
-objective(backend::AbstractObjectiveBackend{T}, x::AbstractVector{T}) where T = @error "Objective interface not properly set: $(typeof(backend))"
+objective(backend::PartitionedBackend{T}, x::AbstractVector{T}) where T = error("Objective interface not properly set: $(typeof(backend))")
 
 """
     partitioned_gradient!(backend::AbstractObjectiveBackend{T}, x::AbstractVector{T}, g::AbstractVector{T})
@@ -27,7 +27,7 @@ objective(backend::AbstractObjectiveBackend{T}, x::AbstractVector{T}) where T = 
 Compute the partitioned gradient from `backend` at the point `x` in place of `g`.
 This method is designed for `PartitionedVector{T}<:AbstractVector{T}` (for now, both `x` and `g`).
 """
-partitioned_gradient!(backend::AbstractGradientBackend{T}, x::AbstractVector{T}, g::AbstractVector{T}) where T = @error "Gradient interface not properly set: $(typeof(backend))"
+partitioned_gradient!(backend::PartitionedBackend{T}, x::AbstractVector{T}, g::AbstractVector{T}) where T = error("Gradient interface not properly set: $(typeof(backend))")
 
 """
     partitioned_hessian_prod!(backend::AbstractHprodBackend{T}, x::AbstractVector{T}, v::AbstractVector{T}, Hv::AbstractVector{T})
@@ -35,7 +35,7 @@ partitioned_gradient!(backend::AbstractGradientBackend{T}, x::AbstractVector{T},
 Compute the partitioned Hessian-vector product ∇² f(x) v from `backend` in place of `Hv`.
 This method is designed for `PartitionedVector{T}<:AbstractVector{T}` (`x`, `v` and `Hv`).
 """
-partitioned_hessian_prod!(backend::AbstractHprodBackend{T}, x::AbstractVector{T}, v::AbstractVector{T}, Hv::AbstractVector{T}) where T = @error "Hessian-product interface not properly set: $(typeof(backend))"
+partitioned_hessian_prod!(backend::PartitionedBackend{T}, x::AbstractVector{T}, v::AbstractVector{T}, Hv::AbstractVector{T}) where T = error("Hessian-product interface not properly set: $(typeof(backend))")
 
 include("ObjectiveBackends/NLPObjectiveBackend.jl")
 include("GradientBackends/ElementReverseDiffGradient.jl")
